d65ff46dd395ad1abf7ddb405997771d40b8c260
Jamie Robertson <jamie.robertson@cellosignal.com>
2019-12-09 23:14:38 +0000

    hotfix: 0.2.9 - added transitions

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { useSpring } from "react-spring"
+import { useInView } from 'react-intersection-observer'
 import Socialbar from "../socialbar"
 import { HeroWrapper, Heading, Prefix, Brand, Dot } from "./styles"
-import { Inner } from "../../theme"
+import { Inner } from "../../theme";
 
 interface HeroProps {
   showBrand?: boolean
@@ -25,10 +27,19 @@ interface HeroProps {
 }
 
 const Hero: React.FC<HeroProps> = ({ showBrand = true, data }) => {
-  const { prefix, title, intro, copyNode, link } = data[0]
+  const { prefix, title, intro, copyNode, link } = data[0];
+
+  const [ref, inView] = useInView({
+    rootMargin: '0px 0px',
+  });
+
+  const props = useSpring({
+    opacity: inView ? 1 : 0,
+    transform: inView ? `translate3d(0, 0, 0)` : `translate3d(0, 20px, 0)`
+  });
 
   return (
-    <HeroWrapper>
+    <HeroWrapper ref={ref} style={props}>
       <Inner>
         {showBrand && <Brand />}
         {title && (

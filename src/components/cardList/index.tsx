3ce54f621315918b04179d67fc97bd54c1bc550b
Jamie Robertson <jamie.robertson@cellosignal.com>
2019-12-09 23:10:05 +0000

    reverted animations as buggy with canvas

--- src/components/cardList/index.tsx
+++ src/components/cardList/index.tsx
@@ -1,6 +1,4 @@
 import * as React from "react"
-import { useSpring, animated } from "react-spring"
-import { useInView } from 'react-intersection-observer'
 import Card from "../card"
 import Heading from "../heading"
 import { Container, Inner } from "../../theme"
@@ -12,33 +10,21 @@ interface CardListProps {
 }
 
 const CardList: React.FC<CardListProps> = ({ heading, cards = [] }) => {
-
-  const [ref, inView] = useInView({
-    rootMargin: '-100px 0px',
-  });
-
-  const props = useSpring({
-    opacity: inView ? 1 : 0,
-    transform: inView ? `translate3d(0, 0, 0)` : `translate3d(0, 30px, 0)`
-  });
-
   return (
     <Container style={{ paddingTop: 0 }}>
       <Inner>
-        <animated.div ref={ref} style={props}>
-          {heading && (
-            <Heading level="2" showDot={true}>
-              {heading}
-            </Heading>
-          )}
-          {cards && (
-            <CardsWrapper>
-              {cards.map((item, i) => (
-                <Card key={`card-${i}`} {...item} />
-              ))}
-            </CardsWrapper>
-          )}
-        </animated.div>
+        {heading && (
+          <Heading level="2" showDot={true}>
+            {heading}
+          </Heading>
+        )}
+        {cards && (
+          <CardsWrapper>
+            {cards.map((item, i) => (
+              <Card key={`card-${i}`} {...item} />
+            ))}
+          </CardsWrapper>
+        )}
       </Inner>
     </Container>
   )
--- src/components/cardList/styles.ts
+++ src/components/cardList/styles.ts
@@ -1,9 +1,8 @@
 import styled from "styled-components"
 import breakpoint from "styled-components-breakpoint"
 import { rem } from "polished"
-import { animated } from "react-spring"
 
-const CardsWrapper = styled(animated.div)`
+const CardsWrapper = styled.div`
   display: block;
   position: relative;
 

f95bba343d4b2420165f5b01e3c713ff5c0f8786
Jamie Robertson <jamie.robertson@cellosignal.com>
2020-02-14 22:23:04 +0000

    added intersectionObserver polyfill for unsupported browsers

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+export const onClientEntry = () => {
+  if (!(`IntersectionObserver` in window)) {
+    import(`intersection-observer`)
+    console.log(`# IntersectionObserver is polyfilled!`)
+  }
+}

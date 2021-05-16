1f4af172304c14affd54f87a7d5c86189f5f2074
Jamie Robertson <jamie.robertson@leith.co.uk>
2021-05-16 23:19:08 +0100

    disabled CSP

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,23 +88,23 @@ module.exports = {
     },
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        disableOnDev: true,
-        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
-        mergeScriptHashes: true, // you can disable scripts sha256 hashes
-        mergeStyleHashes: false, // you can disable styles sha256 hashes
-        mergeDefaultDirectives: true,
-        directives: {
-          "script-src": "'self' www.google-analytics.com",
-          "style-src": "'self' 'unsafe-inline' fonts.googleapis.com",
-          "img-src": "'self' data: www.google-analytics.com",
-          "font-src": "'self' fonts.gstatic.com",
-          "connect-src": "'self' www.google-analytics.com"
-          // you can add your directives or override defaults
-        }
-      }
-    }
+    // {
+    //   resolve: `gatsby-plugin-csp`,
+    //   options: {
+    //     disableOnDev: true,
+    //     reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+    //     mergeScriptHashes: true, // you can disable scripts sha256 hashes
+    //     mergeStyleHashes: false, // you can disable styles sha256 hashes
+    //     mergeDefaultDirectives: true,
+    //     directives: {
+    //       "script-src": "'self' www.google-analytics.com",
+    //       "style-src": "'self' 'unsafe-inline' fonts.googleapis.com",
+    //       "img-src": "'self' data: www.google-analytics.com",
+    //       "font-src": "'self' fonts.gstatic.com",
+    //       "connect-src": "'self' www.google-analytics.com"
+    //       // you can add your directives or override defaults
+    //     }
+    //   }
+    // }
   ]
 }

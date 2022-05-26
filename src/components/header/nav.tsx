bebc7c67dc0edadf592a4a05a3ea57c4b871c099
Jamie Robertson <jamie.robertson@leith.co.uk>
2022-05-26 20:14:41 +0100

    feature(resume): Added resume download to navigation

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
-import { Link } from "gatsby";
-import { motion } from "framer-motion";
-import { urlContext } from "@stores/url.context";
-import { prefersReducedMotionContext } from "@stores/reduceMotion.context";
-import config from "@shared";
-import { inHorizontal, chained } from "@animations";
-import { NavList, NavItem } from "./styles";
+import { Link } from "gatsby"
+import { motion } from "framer-motion"
+import { urlContext } from "@stores/url.context"
+import { prefersReducedMotionContext } from "@stores/reduceMotion.context"
+import config from "@shared"
+import { inHorizontal, chained } from "@animations"
+import { NavList, NavItem } from "./styles"
 
 interface NavProps {
   items: [
@@ -13,27 +13,35 @@ interface NavProps {
       url: string
       text: string
       isActive?: boolean
+      type?: string
     }
   ]
-};
+}
 
 const Nav: React.FC<NavProps> = ({ items }) => {
-  const { url } = React.useContext(urlContext);
-  const { reducedMotion } = React.useContext(prefersReducedMotionContext);
+  const { url } = React.useContext(urlContext)
+  const { reducedMotion } = React.useContext(prefersReducedMotionContext)
 
   return (
     <NavList initial="initial" animate="animate" variants={chained}>
-      {items.map((animation, i) => (
-        <NavItem key={`nav-item${i}`}
-          isActive={url.toLowerCase() === items[i].text.toLowerCase()}
-          custom={reducedMotion}
-          variants={inHorizontal}>
-          {items[i].url && <a href={items[i].url}>{items[i].text}</a>}
-        </NavItem>
-      )
-      )}
+      {items.map((item, i) => {
+        return (
+          <NavItem
+            key={`nav-item${i}`}
+            isActive={url.toLowerCase() === item.text.toLowerCase()}
+            custom={reducedMotion}
+            variants={inHorizontal}
+          >
+            {item.url && (
+              <a href={item.url} {...item}>
+                {item.text}
+              </a>
+            )}
+          </NavItem>
+        )
+      })}
     </NavList>
   )
-};
+}
 
-export default Nav;
+export default Nav
